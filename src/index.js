899cb0a7f16847b0ceefc4ffa7bf326db6f9d934
Cat <parachorradasuno@gmail.com>
2025-07-08 09:35:33 +0200

    responsive Nav-burger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css"
+
+const nav_btn = document.getElementById('nav-toggle');
+
+nav_btn.addEventListener('click', () => {
+    document.querySelector('.nav').classList.toggle('nav-vertical');
+    document.querySelector('.hamburger').classList.toggle('open')
+    
+    document.getElementById('nav-close').classList.toggle('nav-close')
+
+    document.querySelector('.logo-container').classList.toggle('logo-close')
+})

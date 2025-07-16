72d7003edb390635cd639034e78211524dd9ed1b
Cat <parachorradasuno@gmail.com>
2025-07-16 16:50:36 +0200

    half sass

---
+++ src copy/index.js
@@ -0,0 +1,22 @@
+import "./styles.css"
+import { nav_toggle } from "./scripts/nav.js";
+import { show_All } from "./scripts/all_projects.js";
+import { Project } from "./scripts/factory_project.js";
+import { add_things } from "./scripts/add_things.js";
+
+const project = Project(1, 'Project 1', '2/8/2025', 'important', 'this is a test project')
+const add_button = document.getElementById('add-btn')
+
+nav_toggle();
+show_All();
+
+add_button.addEventListener('click', () => {
+    const dropdown = document.getElementById('add-div-dropdown')
+    const options = document.querySelectorAll('btn-dropdown')
+
+    dropdown .classList.toggle('inactive')
+    dropdown .classList.toggle('add-div-dropdown')
+
+    
+    add_things()
+})
